f6ad2c65c178364d00738fc2eb24c4b0f7329411
EdwardMiguel <86975348+EdwardMiguel@users.noreply.github.com>
2022-11-06 20:38:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let botonV = document.getElementById("button")
+let cambio = document.getElementById("cambio")
+
+botonV.onclick = function() {
+
+    imgchange.src = "./images/264090826_604077174135814_9160552437027342864_n.png";
+}
+
+botonV.onclick = function() {
+
+    imgchange.src = "./images/imagen1.jfif";
+}
